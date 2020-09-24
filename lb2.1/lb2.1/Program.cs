384dc10f4597e2aae2ad92442c99bca84717a63e
ykrop22 <45872447+ykrop22@users.noreply.github.com>
2020-09-24 16:04:39 +0300

    Add files via upload

---
+++ lb2.1/lb2.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb2._1
+{
+    public class Address
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public string index { get; set; }
+        public string house { get; set; }
+        public string apartment { get; set; }
+
+        public void writeAddress()
+        {
+            Console.WriteLine($"Ваша адреса: {street}, будинок №{house}, кв.{apartment}, {country}, {city}, {index}");
+            Console.Read();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            Console.WriteLine("Ваша країна: ");
+            address.country = Console.ReadLine();
+            Console.WriteLine("Ваше мiсто: ");
+            address.city = Console.ReadLine();
+            Console.WriteLine("Поштовий iндекс: ");
+            address.index = Console.ReadLine();
+            Console.WriteLine("Назва вулицi: ");
+            address.street = Console.ReadLine();
+            Console.WriteLine("Номер будинку: ");
+            address.house = Console.ReadLine();
+            Console.WriteLine("Квартира №: ");
+            address.apartment = Console.ReadLine();
+
+            address.writeAddress();
+
+        }
+
+    }
+    
+    
+}
